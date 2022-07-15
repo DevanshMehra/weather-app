fd0d05b670f1166d6dd52842384ed7456e97d33d
Devansh <devansh@Devanshs-MacBook-Pro.local>
2022-07-16 00:43:08 +0530

    first commit ☀️

---
+++ src/components/Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a2cdbe6f98msh9f9159b68e10d68p188d33jsn721f6eacf0cc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+
