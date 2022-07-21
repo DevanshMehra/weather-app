60402464c27a392001be2cc1630c417ed4553fa9
Devansh Mehra <devansh2234@gmail.com>
2022-07-21 23:45:03 +0530

    changes in some files

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,9 +1,11 @@
 export const geoApiOptions = {
-	method: 'GET',
+	method: "GET",
 	headers: {
-		'X-RapidAPI-Key': 'a2cdbe6f98msh9f9159b68e10d68p188d33jsn721f6eacf0cc',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
-
+	  "X-RapidAPI-Key": "",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = ""; // enter your key from openweather API
